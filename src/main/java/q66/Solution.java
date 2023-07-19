13d9ac73cf36a524cb42ceec3c3a4186c3ceab02
Qiong Zhou <justin.qiong@gmail.com>
2023-07-19 16:16:03 +0800

    66. 加一 参考官方解答

--- src/main/java/q66/Solution.java
+++ src/main/java/q66/Solution.java
@@ -3,25 +3,20 @@ package q66;
 class Solution {
 
     public int[] plusOne(int[] digits) {
-        digits[digits.length - 1]++;
-        for (int i = digits.length - 1; i >= 0; i--) {
-            int digit = digits[i];
-            if (digit == 10) {
-                if (i != 0) {
-                    digits[i - 1]++;
-                    digits[i] = 0;
-                } else {
-                    digits[i] = 0;
-                    int[] newDigits = new int[digits.length + 1];
-                    newDigits[0] = 1;
-                    for (int j = 1; j < newDigits.length; j++) {
-                        newDigits[j] = digits[j - 1];
-                    }
-                    return newDigits;
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                for (int j = i+1; j < len; j++) {
+                    digits[j] = 0;
                 }
+                return digits;
             }
         }
 
+        // 所有的元素都为9的情况，需要新建一个长度+1的数组
+        digits = new int[len + 1];
+        digits[0] = 1;
         return digits;
     }
 }
