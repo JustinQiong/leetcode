87713c7f15959645b73a0782015c18adb70513e7
Qiong Zhou <justin.qiong@gmail.com>
2023-07-21 10:27:50 +0800

    70. 爬楼梯 动态数组解法

--- src/main/java/q70/Solution.java
+++ src/main/java/q70/Solution.java
@@ -3,10 +3,6 @@ package q70;
 public class Solution {
 
     public int climbStairs(int n) {
-        return climb(n);
-    }
-
-    private int climb(int n) {
         int p = 0;
         int q = 0;
         int r = 1;
