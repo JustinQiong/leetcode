9d7700c825fa179cb95f29b8295d85bf749a1ee7
Qiong Zhou <justin.qiong@gmail.com>
2023-07-22 18:33:27 +0800

    104. 二叉树的最大深度

---
+++ src/main/java/q104/Solution.java
@@ -0,0 +1,15 @@
+package q104;
+
+import com.TreeNode;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+
+}
