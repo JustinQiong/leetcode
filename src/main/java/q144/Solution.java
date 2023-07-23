91e4baff2431ff6baf4babfb31c9aad822345026
Qiong Zhou <justin.qiong@gmail.com>
2023-07-23 22:02:53 +0800

    144. 二叉树的前序遍历 递归解法

---
+++ src/main/java/q144/Solution.java
@@ -0,0 +1,25 @@
+package q144;
+
+import com.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        traverse(root, list);
+        return list;
+    }
+
+    private void traverse(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        list.add(root.val);
+        traverse(root.left, list);
+        traverse(root.right, list);
+    }
+}
