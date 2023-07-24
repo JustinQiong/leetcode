6c0affb4e51a24cfbeb21e7e33a28940136a7ecd
Qiong Zhou <justin.qiong@gmail.com>
2023-07-24 15:10:37 +0800

    415. 字符串相加

---
+++ src/test/java/q415/SolutionTest.java
@@ -0,0 +1,15 @@
+package q415;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void addStringsTest() {
+        Solution solution = new Solution();
+        assertEquals("134", solution.addStrings("11", "123"));
+        assertEquals("0", solution.addStrings("0", "0"));
+    }
+}
