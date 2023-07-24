11fce967432fa85bc709bda04204c650f12ace8a
Qiong Zhou <justin.qiong@gmail.com>
2023-07-24 15:28:25 +0800

    2600. K 件物品的最大和

---
+++ src/test/java/q2600/SolutionTest.java
@@ -0,0 +1,16 @@
+package q2600;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void kItemsWithMaximumSumTest() {
+        Solution solution = new Solution();
+        assertEquals(2, solution.kItemsWithMaximumSum(3, 2, 0, 2));
+        assertEquals(3, solution.kItemsWithMaximumSum(3, 2, 0, 4));
+        assertEquals(5, solution.kItemsWithMaximumSum(6, 6, 6, 13));
+    }
+}
