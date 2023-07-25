93942c635c75caf5a000aeac875f747c0017c7a2
Qiong Zhou <justin.qiong@gmail.com>
2023-07-25 17:15:56 +0800

    136. 只出现一次的数字 异或解法

---
+++ src/main/java/q136/Solution.java
@@ -0,0 +1,12 @@
+package q136;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+
+        return res;
+    }
+}
