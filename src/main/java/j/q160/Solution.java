bb47c1e870e4e4952ef98f93e01e294f63549b4c
Qiong Zhou <justin.qiong@gmail.com>
2023-07-25 19:36:47 +0800

    160. 相交链表 双指针解法

---
+++ src/main/java/j/q160/Solution.java
@@ -0,0 +1,23 @@
+package j.q160;
+
+import j.com.ListNode;
+
+class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+
+
+}
