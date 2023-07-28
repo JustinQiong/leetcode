bc41967d37476a74054e784a100aff990bf335a1
Qiong Zhou <justin.qiong@gmail.com>
2023-07-28 20:55:43 +0800

    118. 杨辉三角

---
+++ src/test/java/j/q118/SolutionTest.java
@@ -0,0 +1,24 @@
+package j.q118;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void generate() {
+        Solution solution = new Solution();
+        List<List<Integer>> res = solution.generate(5);
+        assertIterableEquals(List.of(1), res.get(0));
+        assertIterableEquals(List.of(1, 1), res.get(1));
+        assertIterableEquals(List.of(1, 2, 1), res.get(2));
+        assertIterableEquals(List.of(1, 3, 3, 1), res.get(3));
+        assertIterableEquals(List.of(1, 4, 6, 4, 1), res.get(4));
+
+        List<List<Integer>> res1 = solution.generate(1);
+        assertIterableEquals(List.of(1), res.get(0));
+    }
+}
