7418406aa96a88ce578f390f4fda6f8be2463ebc
Qiong Zhou <justin.qiong@gmail.com>
2023-08-01 20:48:49 +0800

    228. 汇总区间

---
+++ src/main/java/j/q228/Solution.java
@@ -0,0 +1,27 @@
+package j.q228;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+
+    public List<String> summaryRanges(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int len = nums.length;
+        List<String> res = new LinkedList<>();
+        while (left < len) {
+            while (right < len - 1 && nums[right + 1] == nums[right] + 1) {
+                right++;
+            }
+            if (left == right) {
+                res.add(String.valueOf(nums[left]));
+            } else {
+                res.add(nums[left] + "->" + nums[right]);
+            }
+            right++;
+            left = right;
+        }
+        return res;
+    }
+}
