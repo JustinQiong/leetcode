26a1eda7197ccb22a76d019b5f53008816ca6631
Qiong Zhou <justin.qiong@gmail.com>
2023-08-01 19:57:30 +0800

    226. 翻转二叉树

---
+++ src/main/java/j/q226/Solution.java
@@ -0,0 +1,19 @@
+package j.q226;
+
+import j.com.TreeNode;
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.right = left;
+        root.left = right;
+        invertTree(left);
+        invertTree(right);
+        return root;
+    }
+}
