df5b6e91708897b6eb0394daaa57fca94ccdeb0d
Qiong Zhou <justin.qiong@gmail.com>
2023-08-01 21:28:27 +0800

    231. 2 的幂 计算二进制表示有1个一解法及位运算解法

---
+++ src/test/java/j/q231/SolutionTest.java
@@ -0,0 +1,18 @@
+package j.q231;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void isPowerOfTwo() {
+        Solution solution = new Solution();
+        assertTrue(solution.isPowerOfTwoCountBit(1));
+        assertTrue(solution.isPowerOfTwoCountBit(2));
+        assertTrue(solution.isPowerOfTwoCountBit(16));
+        assertFalse(solution.isPowerOfTwoCountBit(3));
+        assertFalse(solution.isPowerOfTwoCountBit(-2147483648));
+    }
+}
