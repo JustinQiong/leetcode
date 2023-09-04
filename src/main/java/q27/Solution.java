827f6596d7a0fd7f363e7bee91cb046e7bf78a05
Qiong Zhou <justin.qiong@gmail.com>
2023-09-04 13:40:25 +0800

    27. 移除元素 双指针解法

---
+++ src/main/java/q27/Solution.java
@@ -0,0 +1,16 @@
+package q27;
+
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int curr = 0;
+        while (curr < nums.length) {
+            if (nums[curr] != val) {
+                nums[i++] = nums[curr];
+            }
+            curr++;
+        }
+        return i;
+    }
+}
