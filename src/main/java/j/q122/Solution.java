842e9a862fe3b1783a0b32bf6de6bd91fbe9446a
Qiong Zhou <justin.qiong@gmail.com>
2023-09-06 15:41:59 +0800

    122. 买卖股票的最佳时机 官方贪心解法

---
+++ src/main/java/j/q122/Solution.java
@@ -0,0 +1,13 @@
+package j.q122;
+
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit += Math.max(0, prices[i] - prices[i - 1]);
+        }
+
+        return maxProfit;
+    }
+}
