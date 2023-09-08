868db66733612d3ccb78f170c1ab9ae8d4729672
Qiong Zhou <justin.qiong@gmail.com>
2023-09-08 10:57:18 +0800

    12. 整数转罗马数字 官方硬编码解法

---
+++ src/main/java/j/q12/Solution.java
@@ -0,0 +1,20 @@
+package j.q12;
+
+public class Solution {
+
+    String[] thousands = {"", "M", "MM", "MMM"};
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(thousands[num / 1000]);
+        sb.append(hundreds[num % 1000 / 100]);
+        sb.append(tens[num%100/10]);
+        sb.append(ones[num%10]);
+        return sb.toString();
+    }
+
+
+}
