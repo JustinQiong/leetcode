e0fbbac352d0ef2b6a86b528661efa4c04f7e6a0
Qiong Zhou <justin.qiong@gmail.com>
2023-09-19 10:03:33 +0800

    137. 只出现一次的数字 II 位运算解法

---
+++ src/test/java/j/q137/SolutionTest.java
@@ -0,0 +1,15 @@
+package j.q137;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void singleNumber() {
+        Solution solution = new Solution();
+        assertEquals(3, solution.singleNumber(new int[]{2, 2, 3, 2}));
+        assertEquals(99, solution.singleNumber(new int[]{0, 1, 0, 1, 0, 1, 99}));
+    }
+}
