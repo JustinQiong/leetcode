3a188774c3b6f79236826a0382b724fa4ff2499f
Qiong Zhou <justin.qiong@gmail.com>
2023-09-21 15:30:13 +0800

    380. O(1) 时间插入、删除和获取随机元素

---
+++ src/main/java/j/q380/RandomizedSet.java
@@ -0,0 +1,44 @@
+package j.q380;
+
+import java.util.*;
+
+public class RandomizedSet {
+
+    private final List<Integer> list;
+    private final Map<Integer, Integer> map;
+    private final Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+        int last = list.get(list.size() - 1);
+        list.set(index, last);
+        list.remove(list.size() - 1);
+        map.put(last, index);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = random.nextInt(list.size());
+        return list.get(randomIndex);
+    }
+}
