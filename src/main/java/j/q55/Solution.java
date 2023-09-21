924c438cf3e356534afca10c8780af960ac75717
Qiong Zhou <justin.qiong@gmail.com>
2023-09-21 12:16:44 +0800

    55 跳跃游戏 动态规划解法

---
+++ src/main/java/j/q55/Solution.java
@@ -0,0 +1,17 @@
+package j.q55;
+
+public class Solution {
+
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reach) {
+                return false;
+            }
+
+            reach = Math.max(i+nums[i], reach);
+        }
+
+        return true;
+    }
+}
