c8fef89fd886678862bdb2b7e3c12952aaa206dd
Qiong Zhou <justin.qiong@gmail.com>
2023-09-28 10:59:10 +0800

    49. 字母异位词分组 排序+哈希解法

---
+++ src/main/java/j/q49/Solution.java
@@ -0,0 +1,19 @@
+package j.q49;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chs = str.toCharArray();
+            Arrays.sort(chs);
+            String key = new String(chs);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
