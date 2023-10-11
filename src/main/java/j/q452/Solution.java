030a63c306a23ce241b6a181d4eafdf237745a60
Qiong Zhou <justin.qiong@gmail.com>
2023-10-11 11:58:57 +0800

    452. 用最少数量的箭引爆气球 排序后贪心解法

---
+++ src/main/java/j/q452/Solution.java
@@ -0,0 +1,23 @@
+package j.q452;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int right = points[0][1];
+        int ans = 1;
+        for (int[] point : points) {
+            if (point[0] > right) {
+                right = point[1];
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
