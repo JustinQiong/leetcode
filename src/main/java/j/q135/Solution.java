c29a6f50a81ac6642cbd8cffbc0c21f898f7b358
Qiong Zhou <justin.qiong@gmail.com>
2023-10-23 10:05:50 +0800

    135. 分发糖果
    先从左到右遍历并记录各小孩的糖果数
    然后从右往左遍历并记录小孩的糖果数,二者取较大值即为该点的糖果数
    
    然后累加得到总的糖果数

--- src/main/java/j/q135/Solution.java
+++ src/main/java/j/q135/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] left = new int[n];
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             if (i > 0 && ratings[i] > ratings[i - 1]) {
                 left[i] = left[i - 1] + 1;
             } else {
@@ -13,7 +13,7 @@ public class Solution {
             }
         }
         int ret = 0, right = 0;
-        for (int i = n - 1; i >=3 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (i < n - 1 && ratings[i] > ratings[i + 1]) {
                 right++;
             } else {
