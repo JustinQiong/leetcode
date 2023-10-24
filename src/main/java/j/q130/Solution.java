51f8f46bfd4ca90ec9fa88c63ee6190acb6abcf0
Qiong Zhou <justin.qiong@gmail.com>
2023-10-24 13:19:39 +0800

    130. 被围绕的区域
    深度优先解法
    
    从最外圈的O往其临近的O进行深度优先搜索, 标记为A
    
    则其他未被标记为A的O就是被X包围的O
    
    遍历所有的网格,将A标记为O,非A的标记为X即可

---
+++ src/main/java/j/q130/Solution.java
@@ -0,0 +1,47 @@
+package j.q130;
+
+public class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+        int row = board.length;
+        int col = board[0].length;
+
+        for (int i = 0; i < row; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, col - 1);
+        }
+
+        for (int i = 1; i < col - 1; i++) {
+            dfs(board, 0, i);
+            dfs(board, row - 1, i);
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        if (row < 0 || col < 0 || row >= rows || col >= cols || board[row][col] != 'O') {
+            return;
+        }
+
+        board[row][col] = 'A';
+        dfs(board, row + 1, col);
+        dfs(board, row - 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
+    }
+}
