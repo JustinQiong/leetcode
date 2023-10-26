93847a343ab4507cd021833e8a8fefda451cb6ae
Qiong Zhou <justin.qiong@gmail.com>
2023-10-26 10:56:49 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置
    两次二分搜索,第一次搜索目标值出现的第一个位置
    第二次搜索目标值出现的最后一个位置
    
    搜索第一个位置时如果遇到等于目标值的情况, 则记录当前位置为第一个位置, 将右区间向左移1,继续找等于的位置,并更新第一个位置
    
    搜索最后一个位置时如果遇到等于目标值的情况,则记录当前位置为最后一个位置,将左区间向右移1,继续找等于的位置,并更新最后一个位置

---
+++ src/main/java/j/q34/Solution.java
@@ -0,0 +1,35 @@
+package j.q34;
+
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = -1, last = -1;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return new int[]{first, last};
+    }
+}
