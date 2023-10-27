d98d022aed830d8b7d1ea6d391200b3a7eb859c8
Qiong Zhou <justin.qiong@gmail.com>
2023-10-27 09:44:57 +0800

    224. 基本计算器
    由于存在括号,所有维护一个当前层级的符号的栈
    遇到左括号将符号入栈,遇到右括号将符号出栈
    栈顶始终是当前层级的符号,遇到-号将当前符号反转,遇到+号保持当前符号不变,遇到数字进行拼接计算
    
    最终返回总和

---
+++ src/main/java/j/q224/Solution.java
@@ -0,0 +1,43 @@
+package j.q224;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int calculate(String s) {
+        Stack<Integer> ops = new Stack<>();
+        ops.push(1);
+        int ret = 0;
+        int sign = 1;
+
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                int num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = 10 * num + (s.charAt(i) - '0');
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+
+        return ret;
+    }
+
+}
