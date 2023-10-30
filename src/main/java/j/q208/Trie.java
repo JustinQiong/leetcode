96d2e1ca83d93760f54afc2f01566ec72adf6e88
Qiong Zhou <justin.qiong@gmail.com>
2023-10-30 10:47:57 +0800

    208. 实现Trie(前缀树)
    树节点的子节点为26个子节点的数组,子节点的索引为 字母-'a', 节点上带是否是末尾的标识
    
    搜索时从根节点开始搜索,遇到null时返回
    
    插入时从跟节点开始搜索,遇到null时插入节点,末尾节点标记

---
+++ src/main/java/j/q208/Trie.java
@@ -0,0 +1,46 @@
+package j.q208;
+
+class Trie {
+
+    Trie[] children;
+    boolean isEnd;
+
+    public Trie() {
+        this.children = new Trie[26];
+        this.isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new Trie();
+            }
+            node = node.children[idx];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = find(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return find(prefix) != null;
+    }
+
+    private Trie find(String target) {
+        Trie node = this;
+        for (int i=0; i< target.length();i++) {
+            int idx = target.charAt(i) - 'a';
+            if (node.children[idx] == null) {
+                return null;
+            }
+            node = node.children[idx];
+        }
+
+        return node;
+    }
+}
