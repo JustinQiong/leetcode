8fb592b7b0ccc09cec525d1c4af71c8c840c1843
Qiong Zhou <justin.qiong@gmail.com>
2023-10-31 09:54:59 +0800

    17. 电话号码和字母组合
    广度优先搜索
    
    维护一个队列,遍历字符串每个字符
    
    如果对列为空,将当前位入队.
    不为空的时候将字符出队,然后和当前数字对应的字符组合,入队.依此类推.

---
+++ src/main/java/j/q17/Solution.java
@@ -0,0 +1,46 @@
+package j.q17;
+
+import java.util.*;
+
+public class Solution {
+
+    private Map<Character, String> phoneMap = new HashMap<>() {{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+    }};
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return Collections.emptyList();
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        int n = digits.length();
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                char ch = digits.charAt(i);
+                String start = phoneMap.get(ch);
+                for (char c : start.toCharArray()) {
+                    queue.offer(String.valueOf(c));
+                }
+            } else {
+                int size = queue.size();
+                for (int j = 0; j < size; j++) {
+                    String str = queue.poll();
+                    char next = digits.charAt(i);
+                    String choices = phoneMap.get(next);
+                    for (char co : choices.toCharArray()) {
+                        queue.offer(str + co);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(queue);
+    }
+}
