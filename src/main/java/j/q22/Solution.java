a15cf5332dc9ea24d3e985616ffd1c021ce0cf30
Qiong Zhou <justin.qiong@gmail.com>
2023-11-01 08:58:31 +0800

    22. 括号生成
    深度优先搜索解法
    
    记录当前左括号和右括号数量
    
    当左括号小于最大括号对数时,可以选择左括号添加或者不添加2条路径.
    
    当右括号小于左括号数时,可以选择添加或者不添加2条路径.
    
    递归在括号树等于最大括号对数*2时记录结果并退出.

---
+++ src/main/java/j/q22/Solution.java
@@ -0,0 +1,37 @@
+package j.q22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    private List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return res;
+        }
+
+        dfs(new StringBuilder(), 0, 0, n);
+        return res;
+    }
+
+    private void dfs(StringBuilder sb, int open, int close, int max) {
+        if (sb.length() == max * 2) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < max) {
+            sb.append('(');
+            dfs(sb, open + 1, close, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (close < open) {
+            sb.append(')');
+            dfs(sb, open, close +1, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
