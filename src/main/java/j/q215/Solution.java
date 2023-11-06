31c586a253478015a6663ed31a8efdbc9d4e10d1
Qiong Zhou <justin.qiong@gmail.com>
2023-11-06 11:48:01 +0800

    215. 数组中的第K个最大的元素
    随机选择一个pivot, 然后将数组中的元素一分为三,大于等于和小于pivot的值
    
    如果大于pivot的值数量大于等于k,则继续在大于pivot的数组中寻找第k个值
    
    如果小于pivot的值数量大于 数组总数-k 时, 则继续在小于pivot的数组中寻找 k - (数组元素总数量-小于pivot的数量)
    
    其他的情况表明当前pivot就是第k个元素,直接返回.
    
    递归调用下去

---
+++ src/main/java/j/q215/Solution.java
@@ -0,0 +1,41 @@
+package j.q215;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        return select(list, k);
+    }
+
+    private int select(List<Integer> nums, int k) {
+        List<Integer> large = new ArrayList<>();
+        List<Integer> low = new ArrayList<>();
+
+        Random ran = new Random();
+        int pivot = nums.get(ran.nextInt(k));
+        for (int num : nums) {
+            if (num < pivot) {
+                low.add(num);
+            } else if (num > pivot) {
+                large.add(num);
+            }
+        }
+
+        if (large.size() >= k) {
+            return select(large, k);
+        }
+        if (nums.size() - low.size() < k) {
+            return select(low, k -nums.size() + low.size());
+        }
+
+        return pivot;
+    }
+}
