80f5238d7bc258c38504ec34636a4d622b4937cc
Qiong Zhou <justin.qiong@gmail.com>
2023-12-03 14:06:49 +0800

    1004. Max Consecutive Ones III
    Slide window, check the zeros in window.
    window is between left and right points.
    If zeros > k, left point move right to find a 0 to make the window contains k zeros again.
    Then right point move right again.

---
+++ src/main/java/j/q1004/Solution.java
@@ -0,0 +1,28 @@
+package j.q1004;
+
+public class Solution {
+
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        int zeros = 0;
+        int max = 0;
+        while (right < n) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            while (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/j/q1004/SolutionTest.java
@@ -0,0 +1,18 @@
+package j.q1004;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void longestOnes() {
+        Solution solution = new Solution();
+        assertEquals(6, solution.longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2));
+        assertEquals(10, solution.longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3));
+        assertEquals(1, solution.longestOnes(new int[] {0}, 1));
+        assertEquals(0, solution.longestOnes(new int[] {0}, 0));
+        assertEquals(1, solution.longestOnes(new int[] {1}, 0));
+    }
+}
