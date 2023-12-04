f97f617c113dd950cbfbe1333149306e5ecead9b
Qiong Zhou <justin.qiong@gmail.com>
2023-12-04 11:32:25 +0800

    443. String Compression
    We maintain two pointers, which are write and read pointers.
    
    Write pointer is assigned to the write index of chars.
    
    Read pointer is assigned to the read index of chars.
    
    We assigned read to curr first, and find the next index which is not equals to chars[curr].
    
    The Length will be read-curr.
    
    Then write into the chars with write pointer.

---
+++ src/main/java/j/q443/Solution.java
@@ -0,0 +1,39 @@
+package j.q443;
+
+public class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int write = 0;
+        int read = 0;
+        while (read < n) {
+            int curr = read;
+            while (read < n && chars[read] == chars[curr]) {
+                read++;
+            }
+
+            chars[write++] = chars[curr];
+
+            int count = read - curr;
+            if (count > 1) {
+                int start = write;
+                while (count > 0) {
+                    chars[write++] = (char) (count % 10 + '0');
+                    count /= 10;
+                }
+                reverse(chars, start, write - 1);
+            }
+        }
+
+        return write;
+    }
+
+    private void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
