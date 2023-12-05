b071949dab67a9c240e12e1d001a52f322b623eb
Qiong Zhou <justin.qiong@gmail.com>
2023-12-05 12:35:04 +0800

    1493. Longest subarray of 1s after deleting one element
    Use sliding window.
    
    Maintain two pointers left and right.
    Keep the zeros in the window are greater or equal 0. If zeros are less than 0. Move left and right pointer. Otherwise, move right pointer.
    return right - left - 1, it is the max window size.

---
+++ src/test/java/j/q1493/SolutionTest.java
@@ -0,0 +1,18 @@
+package j.q1493;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void longestSubarray() {
+        Solution solution = new Solution();
+
+        assertEquals(3, solution.longestSubarray(new int[]{1, 1, 0, 1}));
+        assertEquals(5, solution.longestSubarray(new int[]{0, 1, 1, 1, 0, 1, 1, 0, 1}));
+        assertEquals(2, solution.longestSubarray(new int[]{1, 1, 1}));
+        assertEquals(0, solution.longestSubarray(new int[]{1}));
+    }
+}
