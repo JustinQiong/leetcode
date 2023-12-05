fb89b67193676dc471c94c2103a8264a93f46afb
Qiong Zhou <justin.qiong@gmail.com>
2023-12-05 16:50:32 +0800

    700. Search Binary Search Tree
    If root.val == val return root
    if root.val > val root = root.left
    else root = root.right

---
+++ src/main/java/j/q700/Solution.java
@@ -0,0 +1,19 @@
+package j.q700;
+
+import j.com.TreeNode;
+
+public class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            }
+
+            root = val < root.val ? root.left : root.right;
+        }
+
+        return null;
+    }
+
+}
