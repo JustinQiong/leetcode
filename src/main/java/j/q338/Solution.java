9e239082344e597a05df83625f13d82f10ac4fd8
Qiong Zhou <justin.qiong@gmail.com>
2023-12-07 20:30:30 +0800

    338. Counting Bits
    Dynamic programming solution:
    
    if i is an odd, the bits count of i is the bits count of i-1 plus one.
    
    if i is an even, the bits count of i is the same as bits count of i/2.

---
+++ src/main/java/j/q338/Solution.java
@@ -0,0 +1,17 @@
+package j.q338;
+
+public class Solution {
+
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                res[i] = res[i - 1] + 1;
+            } else {
+                res[i] = res[i/2];
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/test/java/j/q338/SolutionTest.java
@@ -0,0 +1,17 @@
+package j.q338;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void countBits() {
+        Solution solution = new Solution();
+        assertArrayEquals(new int[] {0,1,1}, solution.countBits(2));
+        assertArrayEquals(new int[]{0,1,1,2,1,2}, solution.countBits(5));
+        assertArrayEquals(new int[]{0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5
+}, solution.countBits(55));
+    }
+}
