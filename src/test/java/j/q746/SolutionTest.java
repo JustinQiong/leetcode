989f59cf650e4257d011ab3984f2d35677f5fd5a
Qiong Zhou <justin.qiong@gmail.com>
2023-12-07 16:51:40 +0800

    746. Min Cost Climbing Stairs
    Dynamic programming solution:
    
    Use dp[] to store the min cost to each index.
    
    dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
    
    return dp[n]

---
+++ src/test/java/j/q746/SolutionTest.java
@@ -0,0 +1,16 @@
+package j.q746;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void minCostClimbingStairs() {
+        Solution solution = new Solution();
+        assertEquals(15, solution.minCostClimbingStairs(new int[]{10, 15, 20}));
+        assertEquals(6, solution.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        assertEquals(0, solution.minCostClimbingStairs(new int[]{0, 0, 1, 0}));
+    }
+}
