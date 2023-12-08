d4bd81a37c17a4793e68898501b3dc6643ea5506
Qiong Zhou <justin.qiong@gmail.com>
2023-12-08 14:42:36 +0800

    394. Decode String
    Stack solution:
    
    when char is [ or letter, push the char into stack
    when char is digits, find the whole digits and push into stack
    when char is ], pop the char from stack until [. And use the times digits to multiply the char.
    After construction of the string, push it into the stack.
    
    Finally, the string in the stack will be the answer.

---
+++ src/main/java/j/q394/Solution.java
@@ -0,0 +1,58 @@
+package j.q394;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Solution {
+
+    private int point;
+
+    public String decodeString(String s) {
+        point = 0;
+        LinkedList<String> stack = new LinkedList<>();
+        int n = s.length();
+        while (point < n) {
+            char ch = s.charAt(point);
+            if (Character.isDigit(ch)) {
+                String num = findTheNumber(s);
+                stack.addLast(num);
+            } else if (Character.isLetter(ch) || ch == '[') {
+                stack.addLast(String.valueOf(ch));
+                point++;
+            } else {
+                point++;
+                LinkedList<String> sub = new LinkedList<>();
+                while (!"[".equals(stack.peekLast())) {
+                    sub.addLast(stack.removeLast());
+                }
+                Collections.reverse(sub);
+                stack.removeLast();
+                int times = Integer.parseInt(stack.removeLast());
+                StringBuilder sb = new StringBuilder();
+                String sbstr = getString(sub);
+                while (times-- >0) {
+                    sb.append(sbstr);
+                }
+                stack.addLast(sb.toString());
+            }
+        }
+
+        return getString(stack);
+    }
+
+    private String findTheNumber(String s) {
+        StringBuilder sb = new StringBuilder();
+        while (Character.isDigit(s.charAt(point))) {
+            sb.append(s.charAt(point++));
+        }
+        return sb.toString();
+    }
+
+    private String getString(LinkedList<String> v) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : v) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
