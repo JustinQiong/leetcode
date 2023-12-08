cc582f5ecf8f01b6e6d0abbbe50af2508fc50e92
Qiong Zhou <justin.qiong@gmail.com>
2023-12-08 18:53:53 +0800

    119. Pascal's Triangle II
    Maintain one List, calculate i from the end to the start index.
    
    row[i] = row[i]+row[i-1]
    row[i] and row [i-1] are the value from the last row.

---
+++ src/test/java/j/q119/SolutionTest.java
@@ -0,0 +1,16 @@
+package j.q119;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void getRow() {
+        Solution solution = new Solution();
+//        assertArrayEquals(new int[] {1,3,3,1}, solution.getRow(3).toArray());
+//        assertArrayEquals(new int[] {1}, solution.getRow(0));
+//        assertArrayEquals(new int[]{1,1}, solution.getRow(1));
+    }
+}
