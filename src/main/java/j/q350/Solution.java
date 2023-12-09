2c90d71483bac4736fa88126a9a4d24366f354a8
Qiong Zhou <justin.qiong@gmail.com>
2023-12-09 13:23:38 +0800

    350. Intersection of Two Arrays II
    Maintain set=int[] to record num existences of each number.
    
    if nums2 has a number existing in nums1, set[n]--;
    
    Return the numbers exists in both of the arrays.

---
+++ src/main/java/j/q350/Solution.java
@@ -0,0 +1,23 @@
+package j.q350;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] set = new int[1001];
+        for (int n : nums1) {
+            set[n]++;
+        }
+
+        int[] res = new int[1001];
+        int i = 0;
+        for (int n : nums2) {
+            if (set[n] > 0) {
+                res[i++] = n;
+                set[n]--;
+            }
+        }
+
+        return Arrays.copyOf(res, i);
+    }
+}
