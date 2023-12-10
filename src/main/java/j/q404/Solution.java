97835724b11ee6412bde531fe161848bb9c25292
Qiong Zhou <justin.qiong@gmail.com>
2023-12-10 16:49:33 +0800

    404. Left Leaves Sum
    Depth First Search, if the node is a left node and leave node, sum the val of it.

---
+++ src/main/java/j/q404/Solution.java
@@ -0,0 +1,28 @@
+package j.q404;
+
+import j.com.TreeNode;
+
+public class Solution {
+
+    private int res = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        dfs(root, false);
+        return res;
+    }
+
+    public void dfs(TreeNode root, boolean isLeft) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            if (isLeft) {
+                res += root.val;
+            }
+            return;
+        }
+
+        dfs(root.left, true);
+        dfs(root.right, false);
+    }
+}
