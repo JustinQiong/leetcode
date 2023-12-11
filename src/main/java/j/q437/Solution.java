9e22a5d4e5e0051268e67091f6e29f4e4e3aa3ea
Qiong Zhou <justin.qiong@gmail.com>
2023-12-11 14:05:08 +0800

    437. Path Sum III
    Depth first search + prefix sum:
    
    depth first search each node and use a HashMap to store the sum -> occurrence of the pre nodes.
    
    when find sum - targetSum prefix sum existing in HashMap, then find a path match the requirements.
    
    recursively do the same algo on the left and right node of the node.

---
+++ src/main/java/j/q437/Solution.java
@@ -0,0 +1,33 @@
+package j.q437;
+
+import j.com.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1);
+        return dfs(root, 0L, prefix, targetSum);
+    }
+
+    private int dfs(TreeNode root, Long sum, Map<Long, Integer> prefix, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = 0;
+        sum += root.val;
+        res += prefix.getOrDefault(sum - targetSum, 0);
+        prefix.put(sum, prefix.getOrDefault(sum, 0) + 1);
+
+        int left = dfs(root.left, sum, prefix, targetSum);
+        int right = dfs(root.right, sum, prefix, targetSum);
+        res += left + right;
+        prefix.put(sum, prefix.getOrDefault(sum, 0) - 1);
+
+        return res;
+    }
+}
