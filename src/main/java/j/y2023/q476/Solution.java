8679b3b27e7c3f5996eae1795896daa01810292e
Qiong Zhou <justin.qiong@gmail.com>
2023-12-15 09:09:34 +0800

    Added comments of algorithm

--- src/main/java/j/y2023/q476/Solution.java
+++ src/main/java/j/y2023/q476/Solution.java
@@ -1,5 +1,12 @@
 package j.y2023.q476;
 
+/**
+ * 476. Number Complement
+ *
+ * Use lowbit method to find the highest 1 bit.
+ * do n-=(n&-n) until n == 0. We assign the highest 1 bit with 0s followed by to x.
+ * then get the complement code by reverse code of num masked by x-1.
+ */
 public class Solution {
 
     public int findComplement(int num) {
