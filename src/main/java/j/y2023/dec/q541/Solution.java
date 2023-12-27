48640fa9074cc568ac9fa8e6800f7d327d86185b
Qiong Zhou <justin.qiong@gmail.com>
2023-12-27 16:16:26 +0800

    541. Reverse String II
    Move pointer p from left to right.
    
    Reverse chars from p to p+k-1. If p + k > n, reverse chars from p to n-1 instead.
    
    Move pointer p by step of 2k until p is greater or equal than length of string s;
    
    Return the new string composed of the reversed chars array.

--- src/main/java/j/y2023/dec/q541/Solution.java
+++ src/main/java/j/y2023/dec/q541/Solution.java
@@ -3,22 +3,16 @@ package j.y2023.dec.q541;
 public class Solution {
 
     public String reverseStr(String s, int k) {
-        int p = 0;
         int n = s.length();
         char[] chs = s.toCharArray();
-        while (p < n) {
-            if (p + k > n) {
-                reverse(chs, p, n - 1);
-            } else {
-                reverse(chs, p, p + k-1);
-            }
-            p += 2*k;
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(chs, i, Math.min(n, i + k) - 1);
         }
         return new String(chs);
     }
 
     private void reverse(char[] chs, int left, int right) {
-        while (left <= right) {
+        while (left < right) {
             char tmp = chs[right];
             chs[right--] = chs[left];
             chs[left++] = tmp;
